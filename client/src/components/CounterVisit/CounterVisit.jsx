2052bc9e2f1e0f31a2631ee0ce0d5fa92b2cb63b
Ariel-Alegre <arielalegre98@gmail.com>
2024-03-01 13:37:28 -0300

    client

--- client/src/components/CounterVisit/CounterVisit.jsx
+++ client/src/components/CounterVisit/CounterVisit.jsx
@@ -4,7 +4,6 @@ import styles from './CounterVisit.module.css'
 const CounterVisit = () => {
   useEffect(() => {
     const script = document.createElement('script');
-    script.src = 'https://counter11.optistats.ovh/private/counter.js?c=pmrqssl8b8sd599m8xg8ejen6t1zl5jj&down=async';
     script.async = true;
     document.body.appendChild(script);
 
@@ -15,8 +14,8 @@ const CounterVisit = () => {
 
   return (
     <div id="sfcpmrqssl8b8sd599m8xg8ejen6t1zl5jj" className={styles.counter}>
-      <label href="https://www.contadorvisitasgratis.com" title="contador de visitas">
-        <img src="https://counter11.optistats.ovh/private/contadorvisitasgratis.php?c=pmrqssl8b8sd599m8xg8ejen6t1zl5jj" border="0" title="contador de visitas" alt="contador de visitas" />
+      <label href="https://www.contadorvisitasgratis.com" title="contador de visitas" disabled>
+        <img disabled src="https://counter11.optistats.ovh/private/contadorvisitasgratis.php?c=pmrqssl8b8sd599m8xg8ejen6t1zl5jj" border="0" title="contador de visitas" alt="contador de visitas" />
       </label>
     </div>
   );
